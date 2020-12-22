66c3b1a75dffed438c6bd83475c45cd3721c0a0c
bry-anderson <bryananderson1227@gmail.com>
2020-12-23 01:08:21 +0700

    Linked List Assignment

---
+++ 1MergeSortedLinkedList.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct Node{
+    int angka;
+    Node *next;
+}*head , *tail , *curr;
+
+// Create NULL
+Node *createNode(int angka){
+    Node *newNode = (Node*) malloc(sizeof(Node));
+    newNode->angka = angka;
+    newNode->next  = NULL;
+    return newNode;
+}
+
+// Insert
+void pushTail(int angka){
+    Node *temp = createNode(angka);
+    if(!head){
+        head = tail = temp;
+    }
+    else{
+        tail->next = temp;
+        tail = temp;
+    }
+}
+
+// Print
+void printLinkedList(){
+  Node *curr = head;
+  while(curr !=NULL){
+  printf("%d ", curr-> angka);
+    curr= curr-> next;
+  }
+}
+
+// Sort
+void sortLinkedList(){
+  int temp;
+  Node *curr;
+  Node *ptr = head;
+  while(ptr!= NULL){
+    curr = ptr-> next;
+    while(curr!= NULL){
+      if(ptr->angka > curr->angka){
+        temp = ptr->angka;
+        ptr->angka = curr->angka;
+        curr->angka = temp;
+      }
+      curr = curr->next;
+    }
+    ptr = ptr->next;
+  }
+}
+
+// Driver code
+int main(){
+  int n , m , a , b;
+
+  printf("Banyaknya List ke-1 : "); scanf("%d" , &m);
+  printf("Masukan list ke 1 :\n");
+  for(int i=0 ; i<m ; i++){
+    scanf(" %d" , &a);
+    pushTail(a);
+  }
+
+  printf("Banyaknya List ke-2 : "); scanf("%d" , &n);
+  printf("Masukan list ke 2 :\n");
+  for(int i=0 ; i<n ; i++){
+    scanf(" %d" , &b);
+    pushTail(b);
+  }
+  sortLinkedList();
+  printf("List Akhir :\n");
+  printLinkedList();
+printf("\n");
+return 0;
+}
